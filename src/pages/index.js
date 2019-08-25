a9a36569e9800e8837990fbebe969d45838a98ef
Bill Pliske <bpliske@gmail.com>
2019-08-25 13:49:36 -0700

    got homepage and links working better

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,11 +1,12 @@
 import React from "react"
+import { graphql } from "gatsby"
 import { Link } from "gatsby"
 
 import Layout from "../components/layout"
 import Image from "../components/image"
 import SEO from "../components/seo"
 
-const IndexPage = () => (
+const IndexPage = ({data}) => (
   <Layout>
     <SEO title="Home" />
     <h1>Hi people</h1>
@@ -15,7 +16,34 @@ const IndexPage = () => (
       <Image />
     </div>
     <Link to="/page-2/">Go to page 2</Link>
+    {data.allWordpressPost.edges.map(post => (
+      <li>
+        <Link to={`/${post.node.slug}`}>
+          <h1 dangerouslySetInnerHTML={{ __html: post.node.title }} />
+        </Link>
+        <p dangerouslySetInnerHTML={{ __html: post.node.date }}/>
+      </li>
+    ))}
   </Layout>
 )
 
 export default IndexPage
+
+
+export const query = graphql`
+  query {
+    allWordpressPost {
+      edges {
+        node {
+          title
+          excerpt
+          slug
+          author {
+            name
+          }
+          date(formatString: "MMMM DD, YYYY")
+        }
+      }
+    }
+  }
+`
