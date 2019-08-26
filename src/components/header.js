151e764986a5415b46136ead2b8d69475ef1dfbe
Bill Pliske <bpliske@gmail.com>
2019-08-25 20:45:35 -0700

    prettier now working and styled-components installed

--- src/components/header.js
+++ src/components/header.js
@@ -1,18 +1,19 @@
-import { Link } from "gatsby"
-import React from "react"
+import { Link } from "gatsby";
+import React from "react";
+import styled from "styled-components";
 
 const Header = ({ siteTitle }) => (
-  <header>
-    <div>
-      <h1>
-        <Link
-          to='/'>
-          {siteTitle}
-        </Link>
-      </h1>
-    </div>
-  </header>
-)
+    <header>
+        <div>
+            <Title>
+                <Link to="/">{siteTitle}</Link>
+            </Title>
+        </div>
+    </header>
+);
 
+export default Header;
 
-export default Header
+const Title = styled.h1`
+    padding: 20px;
+`;
--- src/components/layout.js
+++ src/components/layout.js
@@ -9,36 +9,29 @@ import React from "react"
 import { useStaticQuery, graphql } from "gatsby"
 
 import Header from "./header"
-import Footer from './footer'
+import Footer from "./footer"
 import "./layout.css"
 
 const Layout = ({ children }) => {
-  const data = useStaticQuery(graphql`
-    query SiteTitleQuery {
-      site {
-        siteMetadata {
-          title
+    const data = useStaticQuery(graphql`
+        query SiteTitleQuery {
+            site {
+                siteMetadata {
+                    title
+                }
+            }
         }
-      }
-    }
-  `)
+    `)
 
-  return (
-    <>
-      <Header siteTitle={data.site.siteMetadata.title} />
-      <div
-        style={{
-          margin: `0 auto`,
-          maxWidth: 960,
-          padding: `0px 1.0875rem 1.45rem`,
-          paddingTop: 0,
-        }}
-      >
-        <main>{children}</main>
-        <Footer />
-      </div>
-    </>
-  )
+    return (
+        <>
+            <Header siteTitle={data.site.siteMetadata.title} />
+            <div>
+                <main>{children}</main>
+            </div>
+            <Footer />
+        </>
+    )
 }
 
 export default Layout
